d356ebcf84520d3407c6ec36c826c1d4534367d6
Fahad Ibnay Heylaal <fahad19@gmail.com>
2014-12-24 23:33:41 +0000

    run-scripts module.

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function (scripts) {
+	if (typeof scripts === 'undefined') {
+		return;
+	}
+
+	return scripts
+		.map(function (script) {
+			return 'npm run ' + script;
+		})
+		.join(' && ');
+};
---
+++ test/test.js
@@ -0,0 +1,13 @@
+/* global describe, it */
+'use strict';
+var assert = require('assert');
+var runScripts = require('../');
+
+describe('run-scripts node module', function () {
+	it('must produce full command', function () {
+		assert.equal(
+			runScripts(['one', 'two', 'three']),
+			'npm run one && npm run two && npm run three'
+		);
+	});
+});
