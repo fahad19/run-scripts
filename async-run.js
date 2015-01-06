bf1771e608f8a43cb3c3a31c3647c7d5445e0236
Fahad Ibnay Heylaal <fahad19@gmail.com>
2015-01-06 13:38:41 +0100

    async runner can accept callbacks.

--- async-run.js
+++ async-run.js
@@ -4,10 +4,19 @@ var async = require('async');
 var run = require('./run');
 
 module.exports = function (scripts) {
+	var finished = function () {
+
+	};
+
+	if (typeof arguments[1] === 'function') {
+		finished = arguments[1];
+	}
+
 	return async.series(scripts.map(function (script) {
 		return function (end) {
 			run(script, function () {
 				end();
+				finished();
 			});
 		};
 	}));
